c68e0172345015e6ae3236be505da44d65e87e66
diegoitaliait <diego.lagosmorales@pagopa.it>
2021-12-31 12:34:54 +0100

    added service connections

---
+++ azure-devops/app-projects/02_service_connection_acr.tf
@@ -0,0 +1,14 @@
+# 🟢 LAB service connection for azure container registry 
+resource "azuredevops_serviceendpoint_azurecr" "azurecr-lab" {
+  depends_on = [azuredevops_project.project]
+
+  project_id     = azuredevops_project.project.id
+  service_endpoint_name = local.srv_endpoint_docker_registry_lab
+
+  resource_group = local.docker_rg_name
+  azurecr_name          = local.docker_registry_name
+
+  azurecr_subscription_name = var.lab_subscription_name
+  azurecr_spn_tenantid      = module.secrets.values["TENANTID"].value
+  azurecr_subscription_id   = module.secrets.values["LAB-SUBSCRIPTION-ID"].value
+}
