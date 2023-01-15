52791737a2d79fc801b555896b2cb967d1aaf59f
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-01-15 20:00:01 +0100

    fix terraform.sh

--- azure-devops/terraform.sh
+++ azure-devops/terraform.sh
@@ -15,7 +15,7 @@ PROJECT=$2
 shift 2
 other="$*"
 BACKEND_CONFIG_PATH="../.env/${PROJECT}_state.tfvars"
-BACKEND_INI_PATH="../.env/prod-backend.ini"
+BACKEND_INI_PATH="../.env/backend.ini"
 TF_VAR_FILE_PATH="../.env/terraform.tfvars"
 
 echo "[INFO] This is the current directory: ${CURRENT_DIRECTORY}"
@@ -38,16 +38,17 @@ subscription=""
 # shellcheck source=/dev/null
 source "${BACKEND_INI_PATH}"
 
+echo "[INFO] Set account on subscription ${subscription}"
 az account set -s "${subscription}"
 
 if echo "init plan apply refresh import output state taint destroy" | grep -w "$ACTION" > /dev/null; then
   if [ "$ACTION" = "init" ]; then
     echo "[INFO] init tf"
-    terraform "$ACTION" -backend-config="${BACKEND_CONFIG_PATH}" "$other"
+    terraform "$ACTION" -backend-config="${BACKEND_CONFIG_PATH}" $other
   elif [ "$ACTION" = "output" ] || [ "$ACTION" = "state" ] || [ "$ACTION" = "taint" ]; then
     # init terraform backend
     terraform init -reconfigure -backend-config="${BACKEND_CONFIG_PATH}"
-    terraform "$ACTION" "$other"
+    terraform "$ACTION" $other
   else
     # init terraform backend
     echo "[INFO] init tf"
